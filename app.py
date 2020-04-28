d7017675299a225ca0df624b34769e995166322c
nephylum <25045705+nephylum@users.noreply.github.com>
2020-04-28 12:14:06 -0700

    here wego

--- app.py
+++ app.py
@@ -143,6 +143,6 @@ def rooms():
     return jsonify(response), 400
 
 
-# Run the program on port 5000
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# # Run the program on port 5000
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
